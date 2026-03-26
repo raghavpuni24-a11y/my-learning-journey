42252fbe2edd7e433daeb68bba8f6e5e9278caac
Raghav <raghavpuni24@gmail.com>
2026-03-26 23:57:44 +0530

    Learned the concept of final keyword

--- Java Tutorials/OOPS/Polymorphism/Overriding.java
+++ Java Tutorials/OOPS/Polymorphism/Overriding.java
@@ -15,5 +15,10 @@ public class Overriding {
         Advcalc x=new Advcalc();
         int r=x.add(4,6);   // 11
         System.out.println(r);
+
+        Advcalc y=new Advcalc();
+        int s=y.add(7,8);   // 11
+        System.out.println(s);
+
     }
 }
