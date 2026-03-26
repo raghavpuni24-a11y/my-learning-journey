40e430d4b460a9f4593f354fbdbff9504a87db76
Raghav <raghavpuni24@gmail.com>
2026-03-26 23:53:55 +0530

    learned the concept of final keyword

---
+++ Java Tutorials/Core_Concepts/finalKeyword.java
@@ -0,0 +1,14 @@
+package Core_Concepts;
+class Student{
+    static final String COLLEGE="RGPV";
+    static final int ID=106;
+}
+public class finalKeyword {
+    public static void main(String[] args) {
+             final int num=10;
+//             num =12; this is unabled b/c can't reassign or redeclare if final keyword is used
+        System.out.println(Student.COLLEGE);
+        System.out.println(Student.ID);
+    }
+
+}
