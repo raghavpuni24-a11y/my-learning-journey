97707ef161394ee3b0ccf95a5a7a7761c4810ed2
Raghav <raghavpuni24@gmail.com>
2026-04-08 23:53:38 +0530

    More in lists, and tuple

--- Python Tutorials/Data_Structures/list_01.py
+++ Python Tutorials/Data_Structures/list_01.py
@@ -10,5 +10,21 @@ friends[1]="Pineapple"
 print(friends[1])
 # Index slicing just like String
 print(friends[0:3])
-friends.append("hey",69)
-print
+friends.append("hey")# append takes exactly one argument
+
+# Operations
+l1=[1,5,67,3,6,9,34]
+l1.sort()
+print(l1)  # [1, 3, 5, 6, 9, 34, 67], now l1 is updated to this
+l2=[1,5,67,3,6,9,34]
+l2.reverse()# reverse the existing list. If reverse is applied to l1 then the updated list will be reversed
+print(l2) # [34, 9, 6, 3, 67, 5, 1]
+l3=[1,5,67,3,6,9,34]
+l3.append(89)
+print(l3)  #[1, 5, 67, 3, 6, 9, 34, 89]
+l4=[1, 5, 67, 3, 6, 9, 34, 89]
+l4.pop(3)# delete 3rd index element
+print(l4)  #[1, 5, 67, 6, 9, 34, 89]
+l5=[1, 5, 67, 3, 6, 9, 34, 89]
+l5.remove(89)
+print(l5)
---
+++ Python Tutorials/Data_Structures/tuples_02.py
@@ -0,0 +1,6 @@
+# tuples are immutable, just the opposite of lists , same indexing
+a=(1,2,3,"Riddhi", 4.56, True,1) 
+print(a)
+x=a.count(1)
+print(x)
+y=a.index("Riddhi")
